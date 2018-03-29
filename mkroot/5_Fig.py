6629c2f07df371575de467daf96506f1e3851db5
LMouSoo <soo9211@gmail.com>
2018-03-29 14:01:10 +0900

    commit test

--- mkroot/5_Fig.py
+++ mkroot/5_Fig.py
@@ -1,24 +1,21 @@
 import ROOT
 import csv
 
-output = ROOT.TFile("r7_fig.root","UPDATE")
+output = ROOT.TFile("../dat/root/r5_fig.root","UPDATE")
 
-f = open("d7_fig.csv",'r')
+f = open("../dat/csv/d5_fig_d_3.csv",'r')
 
 r = csv.reader(f,delimiter=',')
 
-
-hist = ROOT.TH1F("fig_5_3","3km",10000,0,1000)
+hist = ROOT.TH1F("fig_5_3","3km",100000,2000,50000)
 
 for p in r :
     distance = float(p[0])
     time = float(p[1])
+    if distance > 3000.1 :
+        hist.Fill(distance,1)
 
-    distance_hist.Fill(distance,per)
-    time_hist.Fill(time,per)
 
-
-distance_hist.Write()
-time_hist.Write()
+hist.Write()
 
 output.Close()
